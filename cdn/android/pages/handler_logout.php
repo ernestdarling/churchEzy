57a1f3359e57157c4cfb1e06d7fe8c0fcb327fd7
Desmond Obiri-Yeboah <dobiriyeboah@hotmail.com>
2020-05-14 23:31:10 -0400

    Removed unnecessary files

--- cdn/android/pages/handler_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Log User In forever
-$expire = time()-60*60*24*30*12*100;//Set for 100years
-setcookie('MemberID', $member_id, $expire, '/');
-setcookie('ChurchEziIntro', '', $expire, '/');
-//Redirect to Home
-$URL = 'custom_home.php?cid=5';
-header("Location:$URL");
-?>
